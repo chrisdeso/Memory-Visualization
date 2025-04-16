99c709bdfd7e0196ff3e14b96125175d4bfbe574
Xitones <cdesousa89@gmail.com>
2025-04-16 09:29:28 -0500

    tweak memory_wrapper.h stub

--- src/wrappers/memory_wrappers.h
+++ src/wrappers/memory_wrappers.h
@@ -1,5 +1,5 @@
 // Memory Visualization CS3339 SP25 
-// memory_wrapers.h
+// memory_wrappers.h
 //
 // Overrides default C++ memory operators (new, delete) to track heap allocation and deallocation
 //
@@ -9,3 +9,6 @@
 // 	- initMemoryTrack(): Starts tracking system
 // 	- trackerToJson: Generates JSON output for visualization
 //
+
+#ifndef MEMORY_WRAPPERS_H
+#define MEMORY_TRACKER_H
