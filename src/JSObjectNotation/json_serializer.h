454365e633c5b2c823e3b4b5b045b19d1eb9e03f
Xitones <cdesousa89@gmail.com>
2025-05-08 22:38:50 -0500

    copy over json serializer from working session with some adjustments

--- src/JSObjectNotation/json_serializer.h
+++ src/JSObjectNotation/json_serializer.h
@@ -1,11 +1,8 @@
+// CS3339 Memory Visualization Project
 // json_serializer.h
-//
-// Converts memory tracking data created by memory_tracker.h to JSON for use in the visualization (D3.js)
-// Uses timestamps to create timeline of events and memory state snapshots.
-//
-// Classes:
-// 	- JsonSerializer: Transforms memory tracking data into JSON output
-// 	- Different functions to format different data types into JSON
+// This file converts our memory tracking data into JSON format
+// We're using JSON because it's easy to work with in JavaScript for our visualization
+// and it's a standard format we learned about in class
 
 #ifndef JSON_SERIALIZER_H
 #define JSON_SERIALIZER_H
@@ -20,11 +17,14 @@
 
 class JsonSerializer {
 public:
+    // Convert memory tracking data to JSON string
+    // This is the main function that our visualization will use
     std::string serializeMemoryState(const std::unordered_map<void*, MemoryBlock*>& memoryBlocks) const {
         std::stringstream ss;
         ss << "{\n";
         ss << "  \"memory_blocks\": [\n";
         
+        // Loop through all memory blocks and convert them to JSON
         bool first = true;
         for (const auto& pair : memoryBlocks) {
             if (!first) {
@@ -39,6 +39,7 @@ public:
             ss << "      \"allocation_time\": \"" << timeToString(block->allocationTime) << "\",\n";
             ss << "      \"is_deallocated\": " << (block->isDeallocated ? "true" : "false");
             
+            // Only include deallocation time if the block was freed
             if (block->isDeallocated) {
                 ss << ",\n      \"deallocation_time\": \"" << timeToString(block->deallocationTime) << "\"";
             }
@@ -54,6 +55,8 @@ public:
     }
 
 private:
+    // Helper function to convert time points to strings
+    // We format the time to include milliseconds for more precise tracking
     std::string timeToString(const std::chrono::system_clock::time_point& time) const {
         auto time_t = std::chrono::system_clock::to_time_t(time);
         auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
